72a67ab8dd11c81d7757a7cf578e2cbc8787adb5
Lenya Nab <nabiev.lenya@gmail.com>
2025-09-03 04:34:26 +0400

    Add new method Resiter

--- internal/service/mocks/user.go
+++ internal/service/mocks/user.go
@@ -13,3 +13,11 @@ func (m *MockUserService) CreateToken(role model.UserRole) (string, error) {
 	args := m.Called(role)
 	return args.String(0), args.Error(1)
 }
+
+func (m *MockUserService) Register(email string, password string, role model.UserRole) (*model.User, error) {
+	args := m.Called(email, password, role)
+	if user := args.Get(0); user != nil {
+		return user.(*model.User), args.Error(1)
+	}
+	return nil, args.Error(1)
+}
